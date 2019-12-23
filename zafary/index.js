7a4e72d092700fee53fbc0c528af14f2b73560e2
carlossilva2006 <42659405+carlossilva2006@users.noreply.github.com>
2019-12-23 01:26:32 -0300

    Add files via upload

--- zafary/index.js
+++ zafary/index.js
@@ -1,14 +1,23 @@
 
-const boton=window.document.querySelector('#boton');
-const botonlimpiar=window.document.querySelector('#boton-limpiar');
-boton.addEventListener('Click', mostrardato);
+const boton=window.document.querySelector('boton');
+boton.addEventListener('Click' , mostrardato);
 
 function mostrardato(){
-    var email=document.getElementById("email").Value;
-    var psw=document.getElementById("psw").Value;
-    if(email==="ejemplo@gmail.com" && psw==="123456"){
+    const input1=document.querySelector("email").Value;
+    const input2=document.querySelector("psw").Value;
+    const email=  input1.Value ;
+    const  psw =  input2 . Value ;
+    ////
+    consola.log (email);
+    input1.Value =  ' ' ;
+    input1 . focus ();
+    consola.log (pws);
+    input2.value  =  ' ' ;
+    input2.focus ();
+    //////
+    if(email==='ejemplo@gmail.com' && psw==='123456'){
         console.log('puedes ingresar al sistema :D')
-        alert("Bienvenido carlos a zafary")
+        alert("Bienvenido a zafary")
     }else{
         console.log('no puedes ingresar al sistema')
         alert("Email y/o contraseña incorrectos")
