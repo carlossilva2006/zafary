4048fe1cb08c57212cf858f6b63c79b6aa3167e0
carlossilva2006 <42659405+carlossilva2006@users.noreply.github.com>
2019-12-23 00:23:01 -0300

    Add files via upload

--- zafary/index.js
+++ zafary/index.js
@@ -1,12 +1,12 @@
 
-const boton=window.document.querySelector('btn-submit');
+const boton=window.document.querySelector('#boton');
 const botonlimpiar=window.document.querySelector('#boton-limpiar');
-boton.addEventListener("Click",mostrardato);
+boton.addEventListener('Click', mostrardato);
 
 function mostrardato(){
     var email=document.getElementById("email").Value;
     var psw=document.getElementById("psw").Value;
-    if(email==='carlos@gmail.com'&& psw==="123456"){
+    if(email==="ejemplo@gmail.com" && psw==="123456"){
         console.log('puedes ingresar al sistema :D')
         alert("Bienvenido carlos a zafary")
     }else{
